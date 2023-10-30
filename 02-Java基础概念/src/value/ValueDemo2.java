871c99ec10ab9741e36f2fb5f9c6473cb2a30e53
qzh <2806894439@qq.com>
2023-10-30 22:42:23 +0800

    制表符

---
+++ 02-Java基础概念/src/value/ValueDemo2.java
@@ -0,0 +1,8 @@
+package value;
+
+public class ValueDemo2 {
+    public static void main(String[] args){
+        System.out.println("name" + '\t' + "age");
+        System.out.println("tom" + '\t' + "23");
+    }
+}
