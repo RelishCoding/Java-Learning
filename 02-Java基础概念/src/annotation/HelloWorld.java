feef58966f7dd9da06d8d1e63f3700c3c80f716c
qzh <2806894439@qq.com>
2023-10-30 22:43:17 +0800

    注释的使用

---
+++ 02-Java基础概念/src/annotation/HelloWorld.java
@@ -0,0 +1,10 @@
+package annotation;
+
+public class HelloWorld {
+    //main方法，表示程序的主入口
+    public static void main(String[] args){
+		/*输出语句（打印语句）
+		会把小括号里面的内容进行输出打印*/
+        System.out.println("Hello World");
+    }
+}
