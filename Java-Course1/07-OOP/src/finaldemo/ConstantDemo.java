06c05d05849721aae523fef0008045ce6e7d7244
qzh <2806894439@qq.com>
2026-08-31 15:28:47 +0800

    认识常量

---
+++ Java-Course1/07-OOP/src/finaldemo/ConstantDemo.java
@@ -0,0 +1,11 @@
+package finaldemo;
+
+/**
+ * 详解常量
+ */
+public class ConstantDemo {
+    public static void main(String[] args) {
+        System.out.println("黑马程序员智能家居系统");
+        System.out.println(Constant.SYSTEM_NAME);
+    }
+}
